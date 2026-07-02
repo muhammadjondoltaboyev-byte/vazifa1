c25389dc29004a96c91637568a8dd3e13f43e29a
Muhammadjon <muhammadjondoltaboyev@gmail.com>
2026-07-02 14:36:28 +0500

    2-qadam: Arifmetik funksiyalar qo'shildi

--- functions.py
+++ functions.py
@@ -8,3 +8,19 @@ def kvadrat(a):
 
 def kub(a):
     pass
+
+# 2-qadam
+def yigindi(a, b):
+    pass
+
+
+def ayirma(a, b):
+    pass
+
+
+def kopaytma(a, b):
+    pass
+
+
+def bolish(a, b):
+    pass
